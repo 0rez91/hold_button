ef196a33bb3f642b938da02fbb5370301972bff4
Jose Sanchez Robles <josesanxez@gmail.com>
2016-10-03 09:44:48 +0200

    Add the content

---
+++ Assets/checkButton.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class checkButton : MonoBehaviour {
+
+    bool button_control = false;
+    public GameObject check;
+    public void pressed(BaseEventData eventData)
+    {
+        button_control = true;
+    }
+    public void notpressed(BaseEventData eventData)
+    {
+        button_control = false;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (button_control)
+            check.GetComponent<SpriteRenderer>().color = Color.green;
+        else
+            check.GetComponent<SpriteRenderer>().color = Color.red;
+
+    }
+}
